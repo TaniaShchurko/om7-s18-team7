9af4b78d7bfcbbc643d164fe1eca0bb6b0da1d78
TatianaShchurko <tatyana.shchurko@gmail.com>
2022-04-08 20:06:44 +0200

    del

--- author/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.authors_update),
-    path('<int:id>', views.authors_update, name='update_author'),
-    path('list', views.author_list),
-    
-]
