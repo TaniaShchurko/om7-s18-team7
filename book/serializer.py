d2f15acf200833979bbe34507bf7ad6e95731992
Vladyslav <vladyslav.spivakov@gmail.com>
2022-04-10 19:36:10 +0300

    Little changes to book.serializers

--- book/serializer.py
+++ book/serializer.py
@@ -5,14 +5,19 @@ from author.models import Author
 
 class AuthorHyperlinkedRelatedField(serializers.HyperlinkedRelatedField):
 
+    def to_representation(self, instance):
+        return f'{instance.surname} {instance.name} {instance.patronymic}'
+
     def display_value(self, instance):
         return f'{instance.surname} {instance.name} {instance.patronymic}'
 
 
 class BookListSerializer(serializers.HyperlinkedModelSerializer):
+    author = AuthorHyperlinkedRelatedField(many=True, view_name='author:rest_edit_author', queryset=Author.objects.all(), lookup_field='pk')
+
     class Meta:
         model = Book
-        fields = ('name', 'description', 'count', 'author')
+        fields = ('id', 'name', 'description', 'count', 'author')
 
 
 class BookDetailSerializer(serializers.HyperlinkedModelSerializer):
