5b7fbee3493346d1a2d8b7a07c24b9e1cbf94f58
Vladyslav <vladyslav.spivakov@gmail.com>
2022-04-10 19:32:26 +0300

    Slight changes to serializers

--- order/serializers.py
+++ order/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from .models import Order
 from authentication.models import CustomUser
+from book.models import Book
 from datetime import timedelta, datetime
 
+class BookHyperLinkedRelatedField(serializers.HyperlinkedRelatedField):
 
+    def display_value(self, instance):
+        return instance.name
 
 class OrderDetailSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    book = BookHyperLinkedRelatedField(view_name='book:rest_edit_book', queryset=Book.objects.all(),
+                                       lookup_field='pk')
 
     class Meta:
         model = Order
